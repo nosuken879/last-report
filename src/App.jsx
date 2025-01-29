43eb550164db2b17a42444e58a54daf9056a27ba
nosuken879 <chyn20002@g.nihon-u.ac.jp>
2025-01-29 23:57:16 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-//import React, { useState } from 'react';
-
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 
 export default function App() {
-  //const [selectedScene, setSelectedScene] = useState("Home");
-
   return (
     <div>
       <Header />
